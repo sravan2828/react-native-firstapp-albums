42d84d807c9dad0d75c040e91174c63b53d57323
sravan <sravankumar2828@gmail.com>
2017-12-02 22:37:26 +0530

    final commit
    completed albums app

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+export default class App extends Component {
+  componentWillMount() {
+    
+  }
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Header headerText={'Iron Fitness'} />
+        <AlbumList />
+      </View>
+    );
+  }
+}
+
